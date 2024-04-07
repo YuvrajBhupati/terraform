366fa98682b37a1ee3adb2a51d9723abb08578c9
YuvrajBhupati <yuvrajb8@gmail.com>
2024-04-07 11:26:46 +0530

    adding terraform files

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+
+variable "AMI" {
+  default = "ami-051f8a213df8bc089"
+}
+
+variable "EC2_USER" {
+  default = "ec2-user"
+}
---
+++ vpc.tf
@@ -0,0 +1,49 @@
+resource "aws_vpc" "terraform-vpc" {      # creating vpc
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = "true"
+  enable_dns_support   = "true"
+  instance_tenancy     = "default"
+
+  tags = {
+    Name = "terraform-vpc"
+  }
+}
+
+resource "aws_subnet" "terraform-subnet-public-1" {   # creating public subnet
+  vpc_id                  = aws_vpc.terraform-vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = "true" //it makes this a public subnet
+  availability_zone       = "us-east-1a"
+  tags = {
+    Name = "terraform-subnet-public-1"
+  }
+}
+
+
+resource "aws_internet_gateway" "terraform-igw" {   # internet Gateway
+  vpc_id = aws_vpc.terraform-vpc.id
+  tags = {
+    Name = "terraform-igw"
+  }
+}
+
+resource "aws_route_table" "terraform-public-rt" {   # public route table
+  vpc_id = aws_vpc.terraform-vpc.id
+
+  route {
+    //associated subnet can reach everywhere
+    cidr_block = "0.0.0.0/0"
+    //CRT uses this IGW to reach internet
+    gateway_id = aws_internet_gateway.terraform-igw.id
+  }
+
+  tags = {
+    Name = "terraform-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "terraform-crta-public-subnet-1" {   # route table association with public subnet
+  subnet_id      = aws_subnet.terraform-subnet-public-1.id
+  route_table_id = aws_route_table.terraform-public-rt.id
+}
+
